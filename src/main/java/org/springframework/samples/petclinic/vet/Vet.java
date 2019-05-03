71047d221706d6206ebeef40b57f74e19582980f
Nirakh Rastogi <M1043104@mindtree.com>
2019-05-03 16:55:36 +0530

    Updated vet.java

--- src/main/java/org/springframework/samples/petclinic/vet/Vet.java
+++ src/main/java/org/springframework/samples/petclinic/vet/Vet.java
@@ -20,7 +20,6 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
